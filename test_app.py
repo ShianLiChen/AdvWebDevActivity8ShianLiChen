fbacf8de624fe45870da5e3b66b409077f9f05ce
Shian Li Chen <shianli.chen@ontariotechu.net>
2025-11-13 13:25:46 -0500

    Extending API with POST Endpoint

--- test_app.py
+++ test_app.py
@@ -1,5 +1,6 @@
 # test_app.py
 import app
+from flask import json
 
 
 def test_hello():
@@ -7,3 +8,11 @@ def test_hello():
     response = client.get('/hello')
     assert response.status_code == 200
     assert response.get_json() == {"message": "Hello, World!"}
+
+
+def test_echo():
+    client = app.app.test_client()
+    payload = {"msg": "ping"}
+    response = client.post('/echo', json=payload)
+    assert response.status_code == 201
+    assert response.get_json() == payload
